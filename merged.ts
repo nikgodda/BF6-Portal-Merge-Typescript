0d5ea355ec0dfdea4959a7fc03fa4c19759ede84
nikgodda <96114399+nikgodda@users.noreply.github.com>
2025-11-17 22:57:07 +0500

    modified:   merged.ts 	modified:   src/main.ts

--- merged.ts
+++ merged.ts
@@ -76,7 +76,6 @@ const playerManager = new PlayerManager()
 
 // Triggered on main gamemode start/end. Useful for game start setup and cleanup.
 export async function OnGameModeStarted() {
-
     console.log(Date.now())
 
     // modlib test
@@ -85,31 +84,37 @@ export async function OnGameModeStarted() {
 
 // Triggered when player joins the game. Useful for pregame setup, team management, etc.
 export function OnPlayerJoinGame(eventPlayer: mod.Player): void {
-
     const jsPlayer = playerManager.createPlayer(eventPlayer)
 
     console.log('PLAYER JOINED. ID: ', jsPlayer.playerId)
-
 }
 
 // Triggered when player selects their class and deploys into game. Useful for any spawn/start logic.
 export function OnPlayerDeployed(eventPlayer: mod.Player): void {
-
     const jsPlayer = playerManager.getPlayer(mod.GetObjId(eventPlayer))
 
     if (jsPlayer)
         mod.DisplayHighlightedWorldLogMessage(mod.Message(jsPlayer.playerId))
-
 }
 
 // Triggered when a player is damaged, returns same variables as OnPlayerDied. Useful for custom on damage logic and updating custom UI.
-export function OnPlayerDamaged(eventPlayer: mod.Player, eventOtherPlayer: mod.Player, eventDamageType: mod.DamageType, eventWeaponUnlock: mod.WeaponUnlock): void { }
+export function OnPlayerDamaged(
+    eventPlayer: mod.Player,
+    eventOtherPlayer: mod.Player,
+    eventDamageType: mod.DamageType,
+    eventWeaponUnlock: mod.WeaponUnlock
+): void {}
 
 // Triggered on player death/kill, returns dying player, the killer, etc. Useful for updating scores, updating progression, handling any death/kill related logic.
-export function OnPlayerDied(eventPlayer: mod.Player, eventOtherPlayer: mod.Player, eventDeathType: mod.DeathType, eventWeaponUnlock: mod.WeaponUnlock): void { }
+export function OnPlayerDied(
+    eventPlayer: mod.Player,
+    eventOtherPlayer: mod.Player,
+    eventDeathType: mod.DeathType,
+    eventWeaponUnlock: mod.WeaponUnlock
+): void {}
 
 // Triggered when player leaves the game. Useful for clean up logic, team management, etc.
-export function OnPlayerLeaveGame(eventNumber: number): void { }
+export function OnPlayerLeaveGame(eventNumber: number): void {}
 
-export function OnGameModeEnding(): void { }
+export function OnGameModeEnding(): void {}
 
